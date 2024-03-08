147381a56a030dab5f4f857b0de69f6f7f831511
Diego Allies <91248430+diegoallies@users.noreply.github.com>
2024-03-08 15:26:05 +0200

    update ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,6 @@ function App() {
     getPayrolls();
   }, []); // Empty dependency array to run only once
 
-
   const handleEmployeeSelect = (employee) => {
     setSelectedEmployee(employee);
   };
@@ -30,221 +29,230 @@ function App() {
     <>
       <Header />
       <div className="container">
-        {/* Employee List Table */}
-        <h2>Employee List</h2>
-        <table className="Table">
-          <thead>
-            <tr>
-              <th scope="col">Employee #</th>
-              <th scope="col">First Name</th>
-              <th scope="col">Last Name</th>
-              <th scope="col">Salutation</th>
-              <th scope="col">Profile Color</th>
-            </tr>
-          </thead>
-          <tbody>
-            {payrollsList.map((employee) => (
-              <tr
-                key={employee.id}
-                onClick={() => handleEmployeeSelect(employee)}
-              >
-                <th scope="row">{employee.employeeId}</th>
-                <td>{employee.firstName}</td>
-                <td>{employee.lastName}</td>
-                <td>{employee.salutation}</td>
-                <td>{employee.employeeProfileColor}</td>
+        <div className="employee-list">
+          <h2>Employee List</h2>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Employee #</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Salutation</th>
+                <th>Profile Color</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {payrollsList.map((employee) => (
+                <tr
+                  key={employee.id}
+                  onClick={() => handleEmployeeSelect(employee)}
+                >
+                  <td>{employee.employeeId}</td>
+                  <td>{employee.firstName}</td>
+                  <td>{employee.lastName}</td>
+                  <td>{employee.salutation}</td>
+                  <td>{employee.employeeProfileColor}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
 
-        {/* Employee Information Form */}
-        <h2>Employee Information</h2>
         {selectedEmployee && (
-          <div
-            className="form"
-            style={{ paddingBottom: "50px", paddingTop: "50px" }}
-          >
-            <form>
-              <div className="form-wrapper">
-                <div className="left-column">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="First Name"
-                    value={selectedEmployee.firstName || ""}
-                    onChange={(e) =>
-                      setSelectedEmployee({
-                        ...selectedEmployee,
-                        firstName: e.target.value,
-                      })
-                    }
-                  />
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Last Name"
-                    value={selectedEmployee.lastName || ""}
-                    onChange={(e) =>
-                      setSelectedEmployee({
-                        ...selectedEmployee,
-                        lastName: e.target.value,
-                      })
-                    }
-                  />
-                  <select
-                    className="form-control"
-                    value={selectedEmployee.salutation || ""}
-                    onChange={(e) =>
-                      setSelectedEmployee({
-                        ...selectedEmployee,
-                        salutation: e.target.value,
-                      })
-                    }
-                  >
-                    <option value="">Select Salutation</option>
-                    <option value="Mr">Mr</option>
-                    <option value="Miss">Miss</option>
-                    {/* Add other salutation options here */}
-                  </select>
-                  {/* ... Other input fields and radio buttons similarly */}
-                  <div>
-                    <label>Gender:</label>
-                    <div>
-                      <input
-                        type="radio"
-                        id="male"
-                        name="gender"
-                        value="Male"
-                        checked={selectedEmployee.gender === "Male"}
-                        onChange={() =>
-                          setSelectedEmployee({
-                            ...selectedEmployee,
-                            gender: "Male",
-                          })
-                        }
-                      />
-                      <label htmlFor="male">Male</label>
-                    </div>
-                    <div>
-                      <input
-                        type="radio"
-                        id="female"
-                        name="gender"
-                        value="Female"
-                        checked={selectedEmployee.gender === "Female"}
-                        onChange={() =>
-                          setSelectedEmployee({
-                            ...selectedEmployee,
-                            gender: "Female",
-                          })
-                        }
-                      />
-                      <label htmlFor="female">Female</label>
-                    </div>
-                    <div>
-                      <input
-                        type="radio"
-                        id="unspecified"
-                        name="gender"
-                        value="Unspecified"
-                        checked={selectedEmployee.gender === "Unspecified"}
-                        onChange={() =>
-                          setSelectedEmployee({
-                            ...selectedEmployee,
-                            gender: "Unspecified",
-                          })
-                        }
-                      />
-                      <label htmlFor="unspecified">Unspecified</label>
-                    </div>
-                  </div>
-                </div>
-                <div className="right-column">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Full Name"
-                    value={`${selectedEmployee.firstName || ""} ${
-                      selectedEmployee.lastName || ""
-                    }`}
-                    disabled
-                  />
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Gross Salary"
-                    value={selectedEmployee.grossSalary || ""}
-                    onChange={(e) =>
-                      setSelectedEmployee({
-                        ...selectedEmployee,
-                        grossSalary: e.target.value,
-                      })
-                    }
-                  />
-                  <div>
-                    <label>Profile Color:</label>
-                    <input
-                      type="checkbox"
-                      value="Green"
-                      checked={
-                        selectedEmployee.employeeProfileColor === "Green"
-                      }
-                      onChange={() =>
-                        setSelectedEmployee({
-                          ...selectedEmployee,
-                          employeeProfileColor: "Green",
-                        })
-                      }
-                    />{" "}
-                    Green
-                    <input
-                      type="checkbox"
-                      value="Blue"
-                      checked={selectedEmployee.employeeProfileColor === "Blue"}
-                      onChange={() =>
-                        setSelectedEmployee({
-                          ...selectedEmployee,
-                          employeeProfileColor: "Blue",
-                        })
-                      }
-                    />{" "}
-                    Blue
-                    <input
-                      type="checkbox"
-                      value="Red"
-                      checked={selectedEmployee.employeeProfileColor === "Red"}
-                      onChange={() =>
-                        setSelectedEmployee({
-                          ...selectedEmployee,
-                          employeeProfileColor: "Red",
-                        })
-                      }
-                    />{" "}
-                    Red
-                    <input
-                      type="checkbox"
-                      value="Default"
-                      checked={
-                        selectedEmployee.employeeProfileColor === "Default"
-                      }
-                      onChange={() =>
-                        setSelectedEmployee({
-                          ...selectedEmployee,
-                          employeeProfileColor: "Default",
-                        })
-                      }
-                    />{" "}
-                    Default
-                  </div>
-                </div>
-              </div>
-              <button type="submit" className="btn btn-success">
-                Save Changes
-              </button>
-            </form>
+  <div className="employee-information">
+    <h2>Employee Information</h2>
+    <form>
+      <div className="form-wrapper">
+        <div className="left-column">
+          <div className="form-group">
+            <label>First Name:</label>
+            <input
+              type="text"
+              className="form-control"
+              value={selectedEmployee.firstName || ""}
+              onChange={(e) =>
+                setSelectedEmployee({
+                  ...selectedEmployee,
+                  firstName: e.target.value,
+                })
+              }
+            />
           </div>
-        )}
+          <div className="form-group">
+            <label>Last Name:</label>
+            <input
+              type="text"
+              className="form-control"
+              value={selectedEmployee.lastName || ""}
+              onChange={(e) =>
+                setSelectedEmployee({
+                  ...selectedEmployee,
+                  lastName: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div className="form-group">
+            <label>Salutation:</label>
+            <select
+              className="form-control"
+              value={selectedEmployee.salutation || ""}
+              onChange={(e) =>
+                setSelectedEmployee({
+                  ...selectedEmployee,
+                  salutation: e.target.value,
+                })
+              }
+            >
+              <option value="">Select Salutation</option>
+              <option value="Mr">Mr</option>
+              <option value="Miss">Miss</option>
+              {/* Add other salutation options here */}
+            </select>
+          </div>
+          <div className="form-group" style={{ display: "flex", flexDirection: "row" }}>
+            <label>Gender:</label>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <div>
+                <input
+                  type="radio"
+                  id="male"
+                  name="gender"
+                  value="Male"
+                  checked={selectedEmployee.gender === "Male"}
+                  onChange={() =>
+                    setSelectedEmployee({
+                      ...selectedEmployee,
+                      gender: "Male",
+                    })
+                  }
+                />
+                <label htmlFor="male">Male</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  id="female"
+                  name="gender"
+                  value="Female"
+                  checked={selectedEmployee.gender === "Female"}
+                  onChange={() =>
+                    setSelectedEmployee({
+                      ...selectedEmployee,
+                      gender: "Female",
+                    })
+                  }
+                />
+                <label htmlFor="female">Female</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  id="unspecified"
+                  name="gender"
+                  value="Unspecified"
+                  checked={selectedEmployee.gender === "Unspecified"}
+                  onChange={() =>
+                    setSelectedEmployee({
+                      ...selectedEmployee,
+                      gender: "Unspecified",
+                    })
+                  }
+                />
+                <label htmlFor="unspecified">Unspecified</label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right-column">
+          <div className="form-group">
+            <label>Full Name:</label>
+            <input
+              type="text"
+              className="form-control"
+              value={`${selectedEmployee.firstName || ""} ${
+                selectedEmployee.lastName || ""
+              }`}
+              disabled
+            />
+          </div>
+          <div className="form-group">
+            <label>Gross Salary:</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Gross Salary"
+              value={selectedEmployee.grossSalary || ""}
+              onChange={(e) =>
+                setSelectedEmployee({
+                  ...selectedEmployee,
+                  grossSalary: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div className="form-group">
+            <label>Profile Color:</label>
+            <div>
+              <input
+                type="checkbox"
+                value="Green"
+                checked={selectedEmployee.employeeProfileColor === "Green"}
+                onChange={() =>
+                  setSelectedEmployee({
+                    ...selectedEmployee,
+                    employeeProfileColor: "Green",
+                  })
+                }
+              />{" "}
+              Green
+              <input
+                type="checkbox"
+                value="Blue"
+                checked={selectedEmployee.employeeProfileColor === "Blue"}
+                onChange={() =>
+                  setSelectedEmployee({
+                    ...selectedEmployee,
+                    employeeProfileColor: "Blue",
+                  })
+                }
+              />{" "}
+              Blue
+              <input
+                type="checkbox"
+                value="Red"
+                checked={selectedEmployee.employeeProfileColor === "Red"}
+                onChange={() =>
+                  setSelectedEmployee({
+                    ...selectedEmployee,
+                    employeeProfileColor: "Red",
+                  })
+                }
+              />{" "}
+              Red
+              <input
+                type="checkbox"
+                value="Default"
+                checked={selectedEmployee.employeeProfileColor === "Default"}
+                onChange={() =>
+                  setSelectedEmployee({
+                    ...selectedEmployee,
+                    employeeProfileColor: "Default",
+                  })
+                }
+              />{" "}
+              Default
+            </div>
+          </div>
+        </div>
+      </div>
+      <button type="submit" className="btn btn-success">
+        Save Changes
+      </button>
+    </form>
+  </div>
+)}
+
       </div>
     </>
   );
