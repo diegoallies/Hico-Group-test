8535c598cbd802fb42fd9fd916c089f538d4277f
Diego Allies <91248430+diegoallies@users.noreply.github.com>
2024-03-08 14:24:36 +0200

    creating ui form

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import Header from "./components/header";
+
+function App() {
+  return (
+    <>
+     <Header/>
+     <div className="container">
+      <div className="form" style={{ paddingBottom: "50px", paddingTop: "50px"}}>
+      <form>
+        <div className="form-wrapper" style={{ display: "flex" , justifyContent: "space-between"}}>
+          <div style={{ flex: 1 , marginRight: "10px" }}>
+          <input type="text" className="form-control" placeholder="first name" name="firstName"/>
+
+          </div>
+          <div style={{ flex: 1 , marginRight: "10px" }}>
+          <input type="text" className="form-control" placeholder="last name" name="lastName"/>
+          </div>
+          <button type="submit" style={{ width: "200px", marginLeft: "10px" }} className="btn btn-success">+ Add</button>
+
+
+
+        </div>
+      </form>
+
+
+      </div>
+     
+     </div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <div
+        className="container-fluid"
+        style={{ background: "#F18920", padding: "15px 0", color: "#fafafa" }}
+      >
+        <div className="container">
+          <h5 style={{ color: "#fafafa" }}>PAYROLL SYSTEM</h5>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
