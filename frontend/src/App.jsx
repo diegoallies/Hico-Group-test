4ad5f76e1de4ed228580725e8abc80cb1202a7ce
Diego Allies <91248430+diegoallies@users.noreply.github.com>
2024-03-08 15:01:50 +0200

    got data from bckend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/header";
+import axios from "axios";
 
 function App() {
+  const [editMode, setEditMode] = useState(false);
+  const [payrolls_list, setPayrolls_list] = useState([]);
+
+  const getPayrolls = async () => {
+    try {
+      const { data } = await axios.get("api/show/payrolls"); // Added await here
+      setPayrolls_list(data);
+      console.log(payrolls_list, 'payrolls_list');
+    } catch (error) {
+      console.log(error, "the error axios");
+    }
+  };
+  
+
+  useEffect(() => {
+    // getPayrolls();
+    console.log(JSON.stringify(payrolls_list , 2, 1), 'payrolls_list');
+  }, [payrolls_list]);
+  
   return (
     <>
       <Header />
@@ -31,16 +52,58 @@ function App() {
                   name="lastName"
                 />
               </div>
-              <button
-                type="submit"
-                style={{ width: "200px", marginLeft: "10px" }}
-                className="btn btn-success"
-              >
-                + Add
-              </button>
+
+              {editMode ? (
+                <button
+                  type="submit"
+                  style={{ width: "200px", marginLeft: "10px" }}
+                  className="btn btn-success"
+                >
+                  edit
+                </button>
+              ) : (
+                <button
+                  type="submit"
+                  style={{ width: "200px", marginLeft: "10px" }}
+                  className="btn btn-success"
+                >
+                  add
+                </button>
+              )}
             </div>
           </form>
         </div>
+
+        <table className="Table">
+          <tbody>
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">first name</th>
+                <th scope="col">last name</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+          </tbody>
+
+          <tr>
+            <th>
+              <td scope="row"> 1</td>
+              <td>John</td>
+              <td>Doe</td>
+              <td>
+                <i
+                  className="fa-solid fa-pen-to-square"
+                  style={{ color: "green", cursor: "pointer" }}
+                ></i>
+                <i
+                  className="fa-solid fa-trash-can"
+                  style={{ color: "red", cursor: "pointer" }}
+                ></i>
+              </td>
+            </th>
+          </tr>
+        </table>
       </div>
     </>
   );
