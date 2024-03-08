7018d252dc3258d12b34c05d5341a8fc84cc73e0
Diego Allies <91248430+diegoallies@users.noreply.github.com>
2024-03-08 13:29:39 +0200

    backend complete ( for now )

--- backend/controllers/payrollController.js
+++ backend/controllers/payrollController.js
@@ -1,86 +1,123 @@
 const db = require("../db/database.js");
 
-
 //CREATE DATABASE
 exports.createDB = (req, res) => {
-    let q = 'CREATE DATABASE payroll';
-    db.query(q, (err, result) => {
-        if (err) throw err;
-        return res.status(201).json("DB created");
-    })
-}
+  let q = "CREATE DATABASE payroll_database";
+  db.query(q, (err, result) => {
+    if (err) throw err;
+    return res.status(201).json("DB created");
+  });
+};
 
-//CREATE TABLE
+// CREATE TABLE
 exports.createTable = (req, res) => {
-    let q = 'CREATE TABLE payrolllist1(id int AUTO_INCREMENT, firstName VARCHAR(255), lastName VARCHAR(255), PRIMARY KEY(id))';
-    db.query(q, (err, result) => {
-        if (err) throw err;
-        return res.status(201).json("TABLE CREATED");
-    });
-}
+  let q = `
+        CREATE TABLE payroll_list (
+            id INT AUTO_INCREMENT,
+            employeeId INT,
+            firstName VARCHAR(255) COLLATE utf8mb4_general_ci,
+            lastName VARCHAR(255) COLLATE utf8mb4_general_ci,
+            salutation VARCHAR(10) COLLATE utf8mb4_general_ci,
+            employeeProfileColor VARCHAR(7) COLLATE utf8mb4_general_ci, 
+            grossSalary DECIMAL(15, 2),
+            gender VARCHAR(15) COLLATE utf8mb4_general_ci,
+            PRIMARY KEY(id)
+        )
+    `;
 
+  db.query(q, (err, result) => {
+    if (err) throw err;
+    return res.status(201).json("TABLE CREATED");
+  });
+};
 
-//CREATE LIST
+// CREATE Payroll
 exports.createList = (req, res) => {
-    const q = "INSERT INTO payrolllist1 SET ?";
+  const q = "INSERT INTO payroll_list SET ?";
 
-    const { firstName, lastName } = req.body;
+  const {
+    employeeId,
+    firstName,
+    lastName,
+    salutation,
+    employeeProfileColor,
+    grossSalary,
+    gender,
+  } = req.body;
 
-    db.query(q, { firstName, lastName }, (err, result) => {
-        if (err) return res.json(err);
-        return res.status(200).json(result);
-    });
-}
+  const payrollData = {
+    employeeId,
+    firstName,
+    lastName,
+    salutation,
+    employeeProfileColor,
+    grossSalary,
+    gender,
+  };
 
+  db.query(q, payrollData, (err, result) => {
+    if (err) return res.json(err);
+    return res.status(200).json(result);
+  });
+};
 
 //SHOW payrollS
 exports.showpayrolls = (req, res) => {
-    const q = "SELECT * FROM payrolllist1";
+  const q = "SELECT * FROM payroll_list";
 
-    db.query(q, (err, result) => {
-        if (err) return res.json(err);
-        return res.status(200).json(result);
-    });
+  db.query(q, (err, result) => {
+    if (err) return res.json(err);
+    return res.status(200).json(result);
+  });
 };
 
 //SHOW SINGLE payroll
 exports.singlepayroll = (req, res) => {
-    const q = `SELECT * FROM payrolllist1 where id=${req.params.id}`;
+  const q = `SELECT * FROM payroll_list where id=${req.params.id}`;
 
-    db.query(q, (err, result) => {
-        if (err) return res.json(err);
-        return res.status(200).json(result[0]);
-    });
-}
+  db.query(q, (err, result) => {
+    if (err) return res.json(err);
+    return res.status(200).json(result[0]);
+  });
+};
 
-
-//UPDATE payroll
+// UPDATE payroll
 exports.updatepayroll = (req, res) => {
-    const { firstName, lastName } = req.body;
-    // const q = `UPDATE payrolllist1 SET firstName ='${firstName}' lastName ='${lastName}' where id=${req.params.id}`;
-    const q = `UPDATE payrolllist1 SET ? where id=${req.params.id}`;
+  const {
+    employeeId,
+    firstName,
+    lastName,
+    salutation,
+    employeeProfileColor,
+    grossSalary,
+    gender,
+  } = req.body;
 
-    db.query(q, { firstName, lastName }, (err, result) => {
-        if (err) return res.json(err);
-        return res.status(200).json(result);
-    });
-}
+  const q = `
+        UPDATE payroll_list 
+        SET 
+            employeeId = '${employeeId}',
+            firstName = '${firstName}',
+            lastName = '${lastName}',
+            salutation = '${salutation}',
+            employeeProfileColor = '${employeeProfileColor}',
+            grossSalary = '${grossSalary}',
+            gender = '${gender}'
+        WHERE id = ${req.params.id}
+    `;
 
+  db.query(q, (err, result) => {
+    if (err) return res.json(err);
+    return res.status(200).json(result);
+  });
+};
 
 //DELETE SINGLE payroll
 exports.deleteSinglepayroll = (req, res) => {
+  const q = `DELETE FROM payroll_list  WHERE id=${req.params.id}`;
 
-    const q = `DELETE FROM payrolllist1  WHERE id=${req.params.id}`;
-
-    db.query(q, (err, result) => {
-        if (err) return res.json(err);
-        return res.status(200).json({ data: "payroll deleted" });
-    });
-}
-
-
-
-
-
-
-
+  db.query(q, (err, result) => {
+    if (err) return res.json(err);
+    return res.status(200).json({ data: "payroll deleted" });
+  });
+};
