4a3188f1cf70ce5b4cc7dcb021c1ffd9f1d7a4f7
Diego Allies <91248430+diegoallies@users.noreply.github.com>
2024-03-08 14:24:49 +0200

    pretier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,29 +4,44 @@ import Header from "./components/header";
 function App() {
   return (
     <>
-     <Header/>
-     <div className="container">
-      <div className="form" style={{ paddingBottom: "50px", paddingTop: "50px"}}>
-      <form>
-        <div className="form-wrapper" style={{ display: "flex" , justifyContent: "space-between"}}>
-          <div style={{ flex: 1 , marginRight: "10px" }}>
-          <input type="text" className="form-control" placeholder="first name" name="firstName"/>
-
-          </div>
-          <div style={{ flex: 1 , marginRight: "10px" }}>
-          <input type="text" className="form-control" placeholder="last name" name="lastName"/>
-          </div>
-          <button type="submit" style={{ width: "200px", marginLeft: "10px" }} className="btn btn-success">+ Add</button>
-
-
-
+      <Header />
+      <div className="container">
+        <div
+          className="form"
+          style={{ paddingBottom: "50px", paddingTop: "50px" }}
+        >
+          <form>
+            <div
+              className="form-wrapper"
+              style={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <div style={{ flex: 1, marginRight: "10px" }}>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="first name"
+                  name="firstName"
+                />
+              </div>
+              <div style={{ flex: 1, marginRight: "10px" }}>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="last name"
+                  name="lastName"
+                />
+              </div>
+              <button
+                type="submit"
+                style={{ width: "200px", marginLeft: "10px" }}
+                className="btn btn-success"
+              >
+                + Add
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
-
-
       </div>
-     
-     </div>
     </>
   );
 }
