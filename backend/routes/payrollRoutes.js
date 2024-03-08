bf6bccf481b318730add72cf639609ca1d78e1c9
Diego Allies <91248430+diegoallies@users.noreply.github.com>
2024-03-08 11:17:25 +0200

    routes

--- backend/routes/payrollRoutes.js
+++ backend/routes/payrollRoutes.js
@@ -1,8 +1,19 @@
-const express = require("express");
+const express = require('express');
+const { createDB, createTable, createList, showpayrolls, singlepayroll, updatepayroll, deleteSinglepayroll } = require('../controllers/payrollsController');
 const router = express.Router();
-const { createDB } = require("../controllers/payrollController");
 
-//routes
-router.get("/create/database", createDB);
+
+//user routes
+
+// /api/user/create
+router.get('/create/database', createDB);
+router.get('/create/table', createTable);
+router.post('/create/list', createList);
+router.get('/show/payrolls', showpayrolls);
+router.get('/payroll/:id', singlepayroll);
+router.put('/update/payroll/:id', updatepayroll);
+router.delete('/delete/payroll/:id', deleteSinglepayroll);
+
+
 
 module.exports = router;
