1f61355dc1974a52aa905884303fea3816885ea1
Diego Allies <91248430+diegoallies@users.noreply.github.com>
2024-03-12 03:01:03 +0200

    added read me for backend

--- backend/db/database.js
+++ backend/db/database.js
@@ -11,11 +11,9 @@ const db = mysql.createConnection({
 db.connect(async (error) => {
   if (error) throw error;
 
-  // Step 1: Check if the database exists, create if not
   await payrollController.createDatabaseIfNotExists(db);
  
 
-  // Step 2: Use the created database
   db.changeUser({ database: process.env.DB_DATABASE }, (err) => {
     if (err) throw err;
     console.log(`Using database: ${process.env.DB_DATABASE}`);
